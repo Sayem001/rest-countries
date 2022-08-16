cf298890d9bec6dee673da0a677be1d88913c796
Sayem001 <abuu.sayem@gmail.com>
2022-08-16 18:31:55 +0600

    kjvbefbvervberh

--- js/rest-country.js
+++ js/rest-country.js
@@ -5,17 +5,22 @@ const loadCountries = () => {
 }
 
 const displayCountries = countries => {
-    // console.log(countries);
+    // console.log(countries[0]);
 
     const countriesHTML = countries.map(country => getCountryHTML(country));
-    console.log(countriesHTML)
+    // console.log(countriesHTML[10]);
+
+    const container=document.getElementById('countries');
+    container.innerHTML=countriesHTML.join(' ');
 }
 
 const getCountryHTML = country => {
-    return
-    `
-     <div>
-     <h2>${country.capital}</h2>
+    return`
+     <div class='country'>
+     <h2>${country.name.common}</h2>
+     <h3>Capital ${country.capital}</h3>
+     <img src="${country.flags.png}" alt="">
+
      </div>
      `
 }
